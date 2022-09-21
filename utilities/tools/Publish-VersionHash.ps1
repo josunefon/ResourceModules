6e47597b8dbf3cbfc6e674098233e90a0325c835
Felix Borst <felix.borst@microsoft.com>
2022-09-21 11:25:20 +0200

    fixes

--- utilities/tools/Publish-VersionHash.ps1
+++ utilities/tools/Publish-VersionHash.ps1
@@ -7,13 +7,13 @@ $ErrorActionPreference = "Stop"
 Write-Verbose "Creating folder structure" -Verbose
 
 if (!(Get-ChildItem -Path "fileHashes.json" -Erroraction SilentlyContinue)) {
-    New-Item -Path "fileHashes.json"
+    $null = New-Item -Path "fileHashes.json"
 }
 if (!(Get-Content -Path "fileHashes.json")) {
     Add-Content -Path "fileHashes.json" -Value "{}"
 }
 
-mkdir "Publish-VersionHash-temp"
+$null = New-Item "Publish-VersionHash-temp"
 Set-Location "./Publish-VersionHash-temp"
 
 Write-Verbose "Get GitHub release info" -Verbose
@@ -23,7 +23,7 @@ foreach ($release in $response) {
 
     $existingHashes = Get-Content -Path "$PSScriptRoot/fileHashes.json" | ConvertFrom-Json
 
-    if ($existingHashes."$($release.name)") {
+    if ($existingHashes."$($release.tag_name)") {
         Write-Verbose "Found existing release in fileHashes.json. Skipping..." -Verbose
         continue
     }
@@ -31,43 +31,43 @@ foreach ($release in $response) {
     Write-Verbose "Download release zip for version '$($release.tag_name)' and unpack it" -Verbose
     Invoke-RestMethod -Uri $release.zipball_url -OutFile "$($release.tag_name).zip"
     Expand-Archive -LiteralPath "$($release.tag_name).zip" -DestinationPath "$($release.tag_name)"
-    Set-Location "$($release.name)\Azure-ResourceModules-*\"
+    Set-Location "$($release.tag_name)\Azure-ResourceModules-*\"
 
     Write-Verbose "Get all bicep files" -Verbose
     $filter = Get-ChildItem -Recurse -Filter "deploy.bicep"
 
     Write-Verbose "Create folder to store compiled ARM templates and respective hashes" -Verbose
-    mkdir "../Azure-ResourceModules-ARM"
+    $null = New-Item "../Azure-ResourceModules-ARM"
     $moduleHashes = @{}
 
-    foreach ($module in $filter) {
+    foreach ($module in $filter[1..10]) {
 
         if ($module.FullName -match 'constructs') {
             Write-Verbose "Detected construct. Skipping..." -Verbose
             continue
         }
-        $splitPath = ($module.FullName).split("\")
-        $moduleName = $splitPath -match "Microsoft."
-        $subModuleName = $splitPath[-2]
+        #get providername
+        $splitPath = ($module.FullName).split("Microsoft.")
+        $path = ("Microsoft." + $splitPath[-1]).split("\deploy.bicep")[0].replace("\", "/")
+        $jsonPath = $path.Replace("/", "-")
 
-        #TODO: Modulepath is not correct if there is a sub-sub-module existent. (see Microsoft.Web/sites)
-        Write-Verbose "[$($release.name)] - Building '$moduleName-$subModuleName'.." -Verbose
+        Write-Verbose "[$($release.tag_name)] - Building '$path'.." -Verbose
 
-        if (Get-Content "../Azure-ResourceModules-ARM/$moduleName-$subModuleName-deploy.json" -ErrorAction SilentlyContinue) {
+        if (Get-Content "../Azure-ResourceModules-ARM/$jsonPath-deploy.json" -ErrorAction SilentlyContinue) {
             Write-Verbose "ARM file already exists. Skipping" -Verbose
             continue
         }
 
-        az bicep build --file $module.FullName --outfile "../Azure-ResourceModules-ARM/$moduleName-$subModuleName-deploy.json" --no-restore
+        az bicep build --file $module.FullName --outfile "../Azure-ResourceModules-ARM/$jsonPath-deploy.json" --no-restore
 
         #sort json alphabetically
         #lowercase json
         #remove trailing comma
-        if (Get-Content -Path "../Azure-ResourceModules-ARM/$moduleName-$subModuleName-deploy.json" -ErrorAction SilentlyContinue) {
-            $file = Get-Content -Path "../Azure-ResourceModules-ARM/$moduleName-$subModuleName-deploy.json" | ConvertFrom-Json
+        if (Get-Content -Path "../Azure-ResourceModules-ARM/$jsonPath-deploy.json" -ErrorAction SilentlyContinue) {
+            $file = Get-Content -Path "../Azure-ResourceModules-ARM/$jsonPath-deploy.json" | ConvertFrom-Json
         }
         else {
-            Write-Warning "File '$moduleName-$subModuleName-deploy.json' could not be found."
+            Write-Warning "File '$jsonPath-deploy.json' could not be found."
         }
 
         $resources = $file.resources | ConvertTo-Json -Depth 99
@@ -81,15 +81,12 @@ foreach ($release in $response) {
         # Get template hash
         $encodedText = (Get-FileHash -InputStream $stringAsStream -Algorithm SHA256).Hash
 
-        $moduleHashes.Add("$moduleName/$subModuleName", $encodedText)
-
-        $moduleHashes
+        $moduleHashes.Add($path, $encodedText)
     }
 
     Write-Verbose "Trying to read 'fileHashes.json'" -Verbose
     $existingHashes = Get-Content -Path "$PSScriptRoot/fileHashes.json" | ConvertFrom-Json
 
-    $existingHashes
     $existingHashes | Add-Member "$($release.tag_name)" -Type "NoteProperty" -Value $moduleHashes
     "$PSScriptRoot/fileHashes.json"
     $existingHashes | ConvertTo-Json -Depth 99 | Out-File "$PSScriptRoot/fileHashes.json"
