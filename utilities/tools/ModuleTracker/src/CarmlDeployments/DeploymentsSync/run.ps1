6eabdcfb1fab7a9a7a90daa00545be9e8a107aff
Tomaz Mlakar <mlakar.tomaz@outlook.com>
2022-09-23 11:46:28 +0200

    Adding Module Tracker backend
    Adding Module Tracker backend

---
+++ utilities/tools/ModuleTracker/src/CarmlDeployments/DeploymentsSync/run.ps1
@@ -0,0 +1,254 @@
+﻿<#
+.DESCRIPTION
+    This PowerShell script retrieves all deployments from Azure Tenant, Subscriptions and Resource Groups. After that, an hash using SHA-256 algorithm
+    is generated per each deployment using the resources block from the template used. The hashes will be stored in a Storage Account Table.
+
+    Script is intended to be used within Azure Fuction App and requires the following app settings
+    - storageAccountResourceGroup: The name of the Azure Resource Group where Storage Account is deployed.
+    - storageAccountName: The name of the Storage Account which will be used to store the data/table.
+    - storageAccountSubscriptionId: The Azure Subscription Id linked to the Storage Account where the data will be stored.
+    - noTenantLevelTracking: Flag used when Tenant Level deployments are not needed.
+    - noSubscriptionsLevelTracking: Flag used when Subscriptions Level deployments are not needed.
+    - noResourceGroupsLevelTracking: Flag used when Resource Groups Level deployments are not needed.
+#>
+
+# Input bindings are passed in via param block.
+param($Timer)
+
+#region Helper functions
+
+function Get-Subscriptions ($scope) {
+    Write-Host "[Processing $scope] Starting" -Verbose
+    $subscriptions = Get-AzSubscription -WarningAction SilentlyContinue
+    return $subscriptions
+}
+
+function Invoke-StorageAccountDataAdd {
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [object] $StorageAccountSubscriptionId,
+        [Parameter(Mandatory = $true)]
+        [object] $TableObject,
+        [Parameter(Mandatory = $true)]
+        [array] $TableRows,
+        [Parameter(Mandatory = $true)]
+        [string] $Scope
+    )
+
+    Select-AzSubscription -SubscriptionId $StorageAccountSubscriptionId -WarningAction SilentlyContinue | Out-Null
+    Write-Host "[Processing $Scope] Adding $($TableRows.Count) rows to Storage Account" -Verbose
+    foreach ($row in $TableRows) {
+        if (($row.hash).Length -eq 0) {
+            continue
+        }
+
+        if ($row.scopeName) {
+            $scopeFullName = '{0}-{1}' -f $Scope, $row.scopeName
+        } else {
+            $scopeFullName = $Scope
+        }
+        New-StorageAccountTableRow -Table $TableObject -PartitionKey $row.deploymentId -DeploymentName $row.deploymentName -Hash $row.hash -Scope $scopeFullName
+    }
+}
+
+#endregion
+
+#region Init
+
+Write-Host '[Preparation] Reading script parameters from env variables' -Verbose
+
+$storageAccountName = $env:storageAccountName
+$storageAccountResourceGroup = $env:storageAccountResourceGroup
+$storageAccountSubscriptionId = $env:storageAccountSubscriptionId
+$noTenantLevelTracking = [System.Convert]::ToBoolean($env:noTenantLevelTrackingString)
+$noSubscriptionsLevelTracking = [System.Convert]::ToBoolean($env:noSubscriptionsLevelTracking)
+$noResourceGroupsLevelTracking = [System.Convert]::ToBoolean($env:noResourceGroupsLevelTracking)
+
+Write-Host '[Preparation] Parameters:' -Verbose
+Write-Host "[Preparation]  - storageAccountName: $storageAccountName" -Verbose
+Write-Host "[Preparation]  - storageAccountResourceGroup: $storageAccountResourceGroup" -Verbose
+Write-Host "[Preparation]  - storageAccountSubscriptionId: $storageAccountSubscriptionId" -Verbose
+Write-Host "[Preparation]  - noTenantLevelTracking: $noTenantLevelTracking" -Verbose
+Write-Host "[Preparation]  - noSubscriptionsLevelTracking: $noSubscriptionsLevelTracking" -Verbose
+Write-Host "[Preparation]  - noResourceGroupsLevelTracking: $noResourceGroupsLevelTracking" -Verbose
+
+if ($noTenantLevelTracking -eq $true -and $noSubscriptionsLevelTracking -eq $true -and $noResourceGroupsLevelTracking -eq $true) {
+    Write-Host '[Preparation] Please set the flags for tracking purposes. Exiting...' -Verbose
+    return
+}
+
+#endregion
+
+#region Importing modules
+
+Write-Host '[Preparation] Loading modules' -Verbose
+
+Import-Module module-tracker
+
+#endregion
+
+#region Create the Storage Table
+
+Write-Host '[Preparation] Creating/retrieving storage account table object' -Verbose
+Select-AzSubscription -SubscriptionId $storageAccountSubscriptionId -WarningAction SilentlyContinue | Out-Null
+$tableObject = New-StorageAccountTable -StorageAccountName $storageAccountName -ResourceGroup $storageAccountResourceGroup -TableName 'AzureDeployments'
+
+if (-not $tableObject) {
+    Write-Host '[Preparation] storage account table object not available!' -Verbose
+    return;
+}
+#endregion
+
+#region Getting all Tenant deployments
+
+if ($noTenantLevelTracking -eq $false) {
+    $StartTime = $(Get-Date)
+
+    $processedDeployments = 0
+    try {
+        Write-Host '[Processing tenant] Start' -Verbose
+        $azDeployments = Get-AzTenantDeployment
+        Write-Host "[Processing tenant] Processing $($azDeployments.Count) deployments" -Verbose
+
+        foreach ($deployment in $azDeployments) {
+            try {
+                Save-AzTenantDeploymentTemplate -DeploymentName $deployment.DeploymentName -Force | Out-Null
+            } catch {
+                continue
+            }
+            $hash = Get-TemplateHash -TemplatePath "./$($deployment.DeploymentName).json"
+            $tableRows += [PSCustomObject]@{
+                deploymentName = $deployment.DeploymentName
+                deploymentId   = $deployment.Id
+                hash           = $hash
+            }
+            Remove-Item "./$($deployment.DeploymentName).json"
+            $processedDeployments++
+        }
+    } catch {
+        Write-Output "Error: $($_.Exception.Message)"
+        continue
+    }
+    if ($tableRows.Count -gt 0) {
+        Invoke-StorageAccountDataAdd -StorageAccountSubscriptionId $storageAccountSubscriptionId -TableObject $tableObject -TableRows $tableRows -Scope 'tenant'
+    }
+    $elapsedTime = $(Get-Date) - $StartTime
+    $totalTime = '{0:HH:mm:ss}' -f ([datetime]$elapsedTime.Ticks)
+    Write-Host "[Processing tenant] Done, Time spent $($totalTime)" -Verbose
+} else {
+    Write-Host '[Processing tenant] Tenant level tracking is disabled by selected flags' -Verbose
+}
+
+#endregion
+
+#region Getting all Subscriptions deployments
+
+if ($noSubscriptionsLevelTracking -eq $false) {
+    $StartTime = $(Get-Date)
+    $subscriptions = Get-Subscriptions -Scope 'subscription'
+    $tableRows = @()
+
+    $subCount = 0
+    foreach ($sub in $subscriptions) {
+        $subCount++
+        Write-Host "[Processing subscription] Processing subscription $subCount/$($subscriptions.Count)" -Verbose
+        Select-AzSubscription -SubscriptionId $sub -WarningAction SilentlyContinue | Out-Null
+
+        $processedDeployments = 0
+        try {
+            $azDeployments = Get-AzDeployment
+            Write-Host "[Processing subscription] Processing $($azDeployments.Count) deployments" -Verbose
+
+            foreach ($deployment in $azDeployments) {
+                try {
+                    Save-AzDeploymentTemplate -DeploymentName $deployment.DeploymentName -Force | Out-Null
+                } catch {
+                    continue
+                }
+                $hash = Get-TemplateHash -TemplatePath "./$($deployment.DeploymentName).json"
+                $tableRows += [PSCustomObject]@{
+                    deploymentName = $deployment.DeploymentName
+                    deploymentId   = $deployment.Id
+                    hash           = $hash
+                    scopeName      = $sub.Name
+                }
+                Remove-Item "./$($deployment.DeploymentName).json"
+                $processedDeployments++
+            }
+        } catch {
+            Write-Output "Error: $($_.Exception.Message)"
+            continue
+        }
+        break
+    }
+    if ($tableRows.Count -gt 0) {
+        Invoke-StorageAccountDataAdd -StorageAccountSubscriptionId $storageAccountSubscriptionId -TableObject $tableObject -TableRows $tableRows -Scope 'subscription'
+    }
+    $elapsedTime = $(Get-Date) - $StartTime
+    $totalTime = '{0:HH:mm:ss}' -f ([datetime]$elapsedTime.Ticks)
+
+    Write-Host "[Processing subscription] Done, Time spent $($totalTime)" -Verbose
+} else {
+    Write-Host '[Processing subscription] Subscriptions level tracking is disabled by selected flags'
+}
+
+#endregion
+
+#region Getting all Resource Group deployments per each Subscription
+
+if ($noResourceGroupsLevelTracking -eq $false) {
+    $StartTime = $(Get-Date)
+    $subscriptions = Get-Subscriptions -Scope 'resourceGroup'
+    $tableRows = @()
+    $subCount = 0
+
+    foreach ($sub in $subscriptions) {
+        $subCount++
+        Write-Host "[Processing resourceGroup] Processing subscription $subCount/$($subscriptions.Count)" -Verbose
+        $resourceGroups = Get-AzResourceGroup
+        Select-AzSubscription -SubscriptionId $sub -WarningAction SilentlyContinue | Out-Null
+
+        $processedDeployments = 0
+        $rgCount = 0
+        foreach ($rg in $resourceGroups) {
+            try {
+                $rgCount++
+                Write-Host "[Processing resourceGroup] Processing resoucre group $rgCount/$($resourceGroups.Count)" -Verbose
+                $azDeployments = Get-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName
+                Write-Host "[Processing resourceGroup] Processing $($azDeployments.Count) deployments" -Verbose
+
+                foreach ($deployment in $azDeployments) {
+                    try {
+                        Save-AzResourceGroupDeploymentTemplate -ResourceGroupName $rg.ResourceGroupName -DeploymentName $deployment.DeploymentName -Force -ErrorAction Stop | Out-Null
+                    } catch {
+                        continue
+                    }
+                    $hash = Get-TemplateHash -TemplatePath "./$($deployment.DeploymentName).json"
+                    $tableRows += [PSCustomObject]@{
+                        deploymentName = $deployment.DeploymentName
+                        deploymentId   = $rg.ResourceId
+                        hash           = $hash
+                        scopeName      = $rg.ResourceGroupName
+                    }
+                    Remove-Item "./$($deployment.DeploymentName).json"
+                    $processedDeployments++
+                }
+            } catch {
+                Write-Output "Error: $($_.Exception.Message)"
+                continue
+            }
+        }
+    }
+    if ($tableRows.Count -gt 0) {
+        Invoke-StorageAccountDataAdd -StorageAccountSubscriptionId $storageAccountSubscriptionId -TableObject $tableObject -TableRows $tableRows -Scope 'resourceGroup'
+    }
+    $elapsedTime = $(Get-Date) - $StartTime
+    $totalTime = '{0:HH:mm:ss}' -f ([datetime]$elapsedTime.Ticks)
+
+    Write-Host "[Processing resourceGroup] Done, Time spent $($totalTime)" -Verbose
+} else {
+    Write-Host '[Processing resourcGroup] Resource Groups level tracking is disabled by selected flags' -Verbose
+}
+#endregion
---
+++ utilities/tools/ModuleTracker/src/CarmlDeployments/Modules/module-tracker.psm1
@@ -0,0 +1,165 @@
+﻿<#
+.SYNOPSIS
+Get hash of the ARM deployment JSON template using only 'resources' tag
+
+.DESCRIPTION
+Get hash of the ARM deployment JSON template using only 'resources' tag
+
+.PARAMETER TemplatePath
+Required. Path to the ARM JSON template
+
+.EXAMPLE
+Get-TemplateHash -TemplatePath .\deploy.json
+
+#>
+
+function Get-TemplateHash {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $TemplatePath
+    )
+
+    try {
+        $templateContent = Get-Content $TemplatePath -Raw | ConvertFrom-Json -Depth 100
+        # if no resouces tag, quit
+        if ($templateContent.resources.Count -eq 0) {
+            return ''
+        }
+
+        # remove all except resouces tag
+        foreach ($property in $templateContent.PSObject.Properties) {
+            if ($property.Name.ToLower() -ne 'resources') {
+                $templateContent.PSObject.Properties.Remove($property.Name)
+            }
+        }
+        # reorder resources properties alphabetically
+        $templateContent.resources = $templateContent.resources | Select-Object ($templateContent.resources | Get-Member -MemberType NoteProperty).Name
+
+        # create temp file and export
+        $tmpPath = Join-Path $PSScriptRoot ('HASH-{0}.json' -f (New-Guid))
+        $templateContent | ConvertTo-Json -Depth 100 | Out-File $tmpPath
+        if ($PSVersionTable.Platform -eq 'Unix') {
+            unix2dos $tmpPath
+        }
+
+        # create hash
+        $azHash = (Get-FileHash -Path $tmpPath -Algorithm SHA256).Hash
+
+        return $azHash
+    } catch {
+        throw $_
+    } finally {
+        # Remove temp files
+        if ((-not [String]::IsNullOrEmpty($tmpPath)) -and (Test-Path $tmpPath)) {
+            Remove-Item -Path $tmpPath -Force
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Create a new table in the Storage Account provided
+
+.DESCRIPTION
+Create a new table in the Storage Account provided
+
+.PARAMETER StorageAccountName
+Required. Storage account name
+
+.PARAMETER ResourceGroup
+Required. Resource group name
+
+.PARAMETER TableName
+Required. Table name
+
+
+.EXAMPLE
+New-StorageAccountTable -StorageAccountName 'stgstore01' -ResourceGroup 'data-rg' -TableName 'data'
+
+#>
+
+function New-StorageAccountTable {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $StorageAccountName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceGroup,
+
+        [Parameter(Mandatory = $true)]
+        [string] $TableName
+    )
+
+    try {
+        # get storage Account and context
+        $storageAccount = Get-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroup
+        $ctx = $storageAccount.Context
+
+        # create the table
+        $table = (Get-AzStorageTable -Name $TableName -Context $ctx -ErrorAction SilentlyContinue).CloudTable
+        if ($null -eq $table) {
+            $newTable = New-AzStorageTable -Name $TableName -Context $ctx
+            return $newTable.CloudTable
+        }
+        return $table
+    } catch {
+        throw $_
+    }
+}
+
+<#
+.SYNOPSIS
+Adds a new row to the Storage Account table
+
+.DESCRIPTION
+Adds a new row to the Storage Account table
+
+.PARAMETER Table
+Required. Stoage Account Table object
+
+.PARAMETER PartitionKey
+Required. PartitionKey of the row to be added, usualy deployment Id
+
+.PARAMETER DeploymentName
+Required. Name of the deployment
+
+.PARAMETER Hash
+Required. Hash value
+
+.EXAMPLE
+New-StorageAccountTableRow -Table $table -PartitionKey '/subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Resources/deployments/main-bcb4-0fd963ffbd82-z4h2pdgyedqg6/operations/08585380170111792859' -DeploymentName 'main-bcb4-0fd963ffbd82-z4h2pdgyedqg6' -Hash 'B448693A6E15F3E3512E6C6A4C72E34856B28412227184280997579DD3A767CE'
+
+#>
+
+function New-StorageAccountTableRow {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [object] $Table,
+
+        [Parameter(Mandatory = $true)]
+        [string] $PartitionKey,
+
+        [Parameter(Mandatory = $true)]
+        [string] $DeploymentName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Hash,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Scope
+    )
+
+    try {
+        $PartitionKey = $PartitionKey.Replace('/', '.')
+        Add-AzTableRow -Table $Table -PartitionKey $PartitionKey -RowKey $DeploymentName -property @{'Hash' = $Hash; 'Scope' = $Scope } -UpdateExisting | Out-Null
+    } catch {
+        throw $_
+    }
+}
+
+Export-ModuleMember -Function 'Get-TemplateHash'
+Export-ModuleMember -Function 'New-StorageAccountTable'
+Export-ModuleMember -Function 'New-StorageAccountTableRow'
